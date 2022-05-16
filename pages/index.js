036000a98fc4c8c59c66bcac20855abb8747ef3d
Djalalov <edjalalov@outlook.com>
2022-05-16 23:32:12 +0500

    first touches

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+	return (
+		<div className="">
+			<Head>
+				<title>Uz Video</title>
+				{/* 				<link rel="icon" href="/favicon.ico" />*/}
+			</Head>
+
+			{/* Header */}
+			{/* Nav */}
+			{/* Body */}
+		</div>
+	);
+}
