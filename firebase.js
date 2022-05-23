b8d221de3b2fb519af11ffc68e7487882e826f15
Djalalov <edjalalov@outlook.com>
2022-05-24 01:14:45 +0500

    setting firebase

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyC0qB3z7SKLWqVjkR-zeHV_B9SeV9dvhNk",
+	authDomain: "uz-video.firebaseapp.com",
+	projectId: "uz-video",
+	storageBucket: "uz-video.appspot.com",
+	messagingSenderId: "989745829601",
+	appId: "1:989745829601:web:8e6538416fab5bebc8c9a0",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
+
+export default app;
+export { auth, db };
